a5ce1a55371aa99f8b6d8cee0ef5f38a0e6afb6e
Mike Wild <mikeanthonywild@gmail.com>
2015-07-30 14:58:25 +0100

    Create gpuswap.sh

---
+++ gpuswap.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Script to swap AMD/Nvidia drivers on boot. Both drivers must have
+# been installed first, as the script relies on the various libraries
+# being in their default locations. Script works by overriding
+# symlinks on libglx.so, libgl.so and xorg.conf. Once these point to
+# the AMD/Nvidia libraries, hardware-accelerated OpenGL is available.
+
+function use_nvidia {
+    # UPDATE THESE PATHS
+    driver_pkg=nvidia
+    libglx_path=/usr/lib64/xorg/modules/extensions/libglx.so.346.47
+    libgl_path=/usr/lib64/libGL.so.346.47
+    xorg_path=/SW-SCRT-0030/bladeConfig/xorg.conf.K2200M
+}
+
+
+function use_fglrx {
+    # UPDATE THESE PATHS
+    driver_pkg=fglrx
+    libglx_path=/usr/lib64/xorg/modules/extensions/fglrx/fglrx-libglx.so
+    libgl_path=/usr/lib64/fglrx/fglrx-libGL.so.1.2  
+    xorg_path=/SW-SCRT-0030/bladeConfig/xorg/conf.S4000X
+}
+
+
+function swap_drivers {
+    echo "Using $driver_pkg driver"
+    
+    # libglx
+    printf "Linking libglx.so..."
+    if [ -s $libglx_path ]; then
+        ln -snf $libglx_path /usr/lib64/xorg/modules/extensions/libglx.so
+        echo OK
+    else
+        echo ERROR
+        echo "Couldn't find $libglx_path"
+        exit 1
+    fi
+
+    # libGL
+    printf "Linking libGL.so.1..."
+    if [ -s $libgl_path ]; then
+        ln -snf $libgl_path /usr/lib64/libGL.so.1
+        echo OK
+    else
+        echo ERROR
+        echo "Couldn't find $libgl_path"
+        exit 1
+    fi
+
+    # Xorg
+    printf "Linking xorg.conf..."
+    if [ -s $xorg_path ]; then
+        ln -snf $xorg_path /etc/X11/xorg.conf
+        echo OK
+    else
+        echo ERROR
+        echo "Couldn't find $xorg_path"
+        exit 1
+    fi
+}
+
+
+# Main entry point
+kmsg=$(lspci | grep VGA)
+echo $kmsg
+
+if [[ $kmsg == *"NVIDIA"* ]]; then
+    use_nvidia
+elif [[ $kmsg == *"AMD"* ]]; then
+    use_fglrx
+else
+    echo "ERROR: Unsupported VGA controller"
+    exit 1
+fi
+
+swap_drivers
+exit 0
